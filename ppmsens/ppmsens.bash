3f1858a061a5a3a8486aa2071d9f0aed9744b659
root <root@greenho1>
2020-10-24 14:15:32 -0700

    add service

--- ppmsens/ppmsens.bash
+++ ppmsens/ppmsens.bash
@@ -1,4 +1,4 @@
-SOCK=${1:-/dev/ttyUSB2}
+SOCK=${1:-/dev/ttyUSB0}
 
 stty -F $SOCK 115200 raw
 function poll_sensor() {
@@ -13,10 +13,11 @@ while true; do
       done  < $SOCK | jq 'select(.res == "4") | del(.d,.m,.y,.h,.min,.sec)' |  tr -d '":{},' | tr -s ' ' |  cut -d ' ' -f2,3 | tr -s '\n' | \
 	  while read -a REC; do
 	      COL=${REC[0]}
-	      COL=${COL//\./}
+	      COL=${COL//\./_}
+	      echo $COL
 	      if [[ $COL ]]; then
 		 curl -X POST -d @- "http://localhost:8086/write?db=greenhouse" <<EOF 
-$COL,host=clarissa,region=baker30s,room=gh1 $COL=${REC[1]} $(date '+%s')000000000
+$COL,host=clarissa,region=baker30s,room=upstairs $COL=${REC[1]} $(date '+%s')000000000
 EOF
 	      fi
 	  done
